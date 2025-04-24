9b9abc3ffab3b9b79a3c49a8fe228af996a3da02
BOing u <boingu@MacBook-Air--BOing.local>
2025-04-24 16:58:33 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        string name = Console.ReadLine();
+        Console.WriteLine($"Привет {name}");
+
+        while (true)
+        {
+            string cmd = Console.ReadLine();
+
+            if (cmd == "exit")
+            {
+                Console.WriteLine($"До свидания, {name}");
+                break;
+            }
+        }
+    }
+}
