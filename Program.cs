c4ad14b3e686f13e35d55b73a13fdc5d029c8831
BOing u <boingu@MacBook-Air--BOing.local>
2025-04-24 17:27:37 +0300

    updated

--- Program.cs
+++ Program.cs
@@ -2,6 +2,16 @@
 
 class Program
 {
+    private static List<string> jokes = new List<string>()
+    {
+        "- Официант, у меня в супе муха! - Вот и попалась, наконец!",
+        "Новинка книжного рынка \"Мемуары Кота Ученого\", песни и сказки для тех, кто ходит налево и направо...",
+        "Новый \"Фейри\" с ароматом конопли - мой посуду весело!",
+        "Начальник отдела входит в кабинет и орёт на сотрудников: - Я же сказал: во время работы курить запрещается! На что получает ответ: - А кто работает?"
+    };
+    private static List<string> badWordPhrases = new List<string>() { "" };
+    private static List<string> badWords = new List<string>() { "" };
+
     private static void Main(string[] args)
     {
         string name = Console.ReadLine();
@@ -11,11 +21,28 @@ class Program
         {
             string cmd = Console.ReadLine();
 
-            if (cmd == "exit")
+            string[] words = cmd.Split(' ');
+
+            foreach (var i in words)
             {
-                Console.WriteLine($"До свидания, {name}");
-                break;
+                if (badWords.Contains(i))
+                {
+                    int random = 0;
+                    Console.WriteLine(badWordPhrases[random]);
+                    continue; // я не помню как рандом в c# без unity делается
+                }
             }
+
+            string response = cmd switch
+            {
+                "расскажи шутку" => jokes[0], //тут тоже рандом должен быть
+                "пока" => $"До свидания, {name}"
+            };
+
+            Console.WriteLine(response);
+
+            if (cmd == "пока")
+                break;
         }
     }
 }
