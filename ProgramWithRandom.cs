be85d5fbf31aef09e4be380fb163913ca9097ed3
BOing u <boingu@MacBook-Air--BOing.local>
2025-04-24 17:33:08 +0300

    added random

---
+++ ProgramWithRandom.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class Program
+{
+    private static List<string> jokes = new List<string>()
+    {
+        "- Официант, у меня в супе муха! - Вот и попалась, наконец!",
+        "Новинка книжного рынка \"Мемуары Кота Ученого\", песни и сказки для тех, кто ходит налево и направо...",
+        "Новый \"Фейри\" с ароматом конопли - мой посуду весело!",
+        "Начальник отдела входит в кабинет и орёт на сотрудников: - Я же сказал: во время работы курить запрещается! На что получает ответ: - А кто работает?"
+    };
+    private static List<string> badWordPhrases = new List<string>() { "" };
+    private static List<string> badWords = new List<string>() { "" };
+
+    private static void Main(string[] args)
+    {
+        string name = Console.ReadLine();
+        Console.WriteLine($"Привет {name}");
+
+        while (true)
+        {
+            string cmd = Console.ReadLine();
+
+            string[] words = cmd.Split(' ');
+
+            foreach (var i in words)
+            {
+                if (badWords.Contains(i))
+                {
+                    Console.WriteLine(badWordPhrases[new Random().Next(0, badWordPhrases.Count)]);
+                    continue;
+                }
+            }
+
+            string response = cmd switch
+            {
+                "расскажи шутку" => jokes[new Random().Next(0, jokes.Count)],
+                "пока" => $"До свидания, {name}"
+            };
+
+            Console.WriteLine(response);
+
+            if (cmd == "пока")
+                break;
+        }
+    }
+}
